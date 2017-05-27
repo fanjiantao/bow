e771710b5dcc8a1ba81d5c42f9ed43b4c4df2242
fanjiantao <fanjiantao0115@gmail.com>
2017-05-27 16:23:49 +0800

    spring security

---
+++ src/main/java/com/phome/bow/settings/AuthorityConfigImpl.java
@@ -0,0 +1,19 @@
+package com.phome.bow.settings;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import net.engining.pg.support.authority.AuthorityConfigInter;
+import net.engining.pg.support.authority.ModuleAuthority;
+
+@Service
+public class AuthorityConfigImpl implements AuthorityConfigInter {
+
+	@Override
+	public List<ModuleAuthority> authorityConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
